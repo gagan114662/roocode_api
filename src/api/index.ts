9f93ca6ffbdf65342b99620da4fc823f592c25de
gagan114662 <gagan@getfoolish.com>
2025-04-20 18:34:25 -0400

    feat: wire up cost forecast endpoint
    - Mount forecast router in main API
    - Add API versioning prefix
    - Enable JSON request parsing

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import tasksRouter from '../routes/tasks';
+import forecastRouter from '../routes/forecastCost';
 
 const app = express();
 app.use(express.json());
 
 // Mount routes
 app.use('/api/v1/tasks', tasksRouter);
+app.use('/api/v1', forecastRouter);
 
 export default app;
